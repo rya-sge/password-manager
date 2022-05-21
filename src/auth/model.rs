2888852fe6e4ba3c05199cf20abceb18ec21cc98
rya-sge <ryan.sauge@heig-vd.ch>
2022-05-22 01:04:01 +0200

    fix some errors

--- src/auth/model.rs
+++ src/auth/model.rs
@@ -45,8 +45,8 @@ pub fn add_password_database(connectionUser: &Connection, connectionPassword: &C
 
             statement.bind(1, username.as_str().clone()).unwrap();
             statement.bind(2, label.as_str().clone()).unwrap();
-            let bufferString = String::from_utf8(buf).unwrap();
-            statement.bind(3, bufferString.as_str().clone()).unwrap();
+            let bufferString = str::from_utf8(buf).unwrap();
+            statement.bind(3, bufferString).unwrap();
             let result = statement.next();
             match result {
                 Ok(e) => {
